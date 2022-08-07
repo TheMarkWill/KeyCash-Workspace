cab638734de1ee4bfe5aa0ce48f40af9692d2643
The Mark <marcon@keycash.io>
2022-08-06 22:59:16 -0300

    Fix errors in devContainer

--- .devcontainer-alternative/pos-build.sh
+++
@@ -1,6 +0,0 @@
-npm install -g git-draft
-npm install -g eslint
-npm install -g vercel
-npm install -g typescript
-npm install -g ts-node
-npm install -g ts-node-dev
---
+++ .devcontainer/post-build.sh
@@ -0,0 +1,1 @@
+npm install -g git-draft eslint vercel typescript ts-node ts-node-dev
